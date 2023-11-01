69b8f99398bfeccafd0e7c3bcd7d6e0649c9d206
Lowell Bacalso <bacalso318@gmail.com>
2023-11-02 00:30:55 +0800

    try

--- Joblynx/src/pages/second-page/index.js
+++ Joblynx/src/pages/second-page/index.js
@@ -16,7 +16,7 @@ const SecondPage = () => {
             <Typography>
               Chocolate sesame snaps pie carrot cake pastry pie lollipop muffin.
               Carrot cake dragée chupa chups jujubes. Macaroon liquorice cookie
-              wafer tart marzipan bonbon. Gingerbread jelly-o dragée chocolate.
+              wafer tart marzipan bonbon. Gingerbread jelly-o dragée chocolate. I add changes
             </Typography>
           </CardContent>
         </Card>
