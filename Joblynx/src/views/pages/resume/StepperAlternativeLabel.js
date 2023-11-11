ebcaaaaa9acc5b015615d322976eb5f6101c2aae
Lowell Bacalso <bacalso318@gmail.com>
2023-11-11 16:23:21 +0800

    Resume form next to finish

--- Joblynx/src/views/pages/resume/StepperAlternativeLabel.js
+++ Joblynx/src/views/pages/resume/StepperAlternativeLabel.js
@@ -1,6 +1,5 @@
 // ** React Imports
-import { Fragment, forwardRef,  useState } from 'react'
-
+import { Fragment, forwardRef, useState } from 'react'
 
 // ** MUI Imports
 
@@ -25,7 +24,6 @@ import FormLabel from '@mui/material/FormLabel'
 import FormControl from '@mui/material/FormControl'
 import FormControlLabel from '@mui/material/FormControlLabel'
 
-
 // ** Icon Imports
 import Icon from 'src/@core/components/icon'
 
@@ -37,7 +35,6 @@ import toast from 'react-hot-toast'
 
 // ** Styled Component
 import StepperWrapper from 'src/@core/styles/mui/stepper'
-
 
 // ** Third Party Imports
 import DatePicker from 'react-datepicker'
@@ -58,6 +55,10 @@ const steps = [
     subtitle: 'Show your relevant experience'
   },
   {
+    title: 'Background',
+    subtitle: 'Show your background experience'
+  },
+  {
     title: 'Social Links',
     subtitle: 'Add Social Links'
   }
@@ -66,9 +67,9 @@ const StepperAlternativeLabel = () => {
   // ** States
   const [email, setEmail] = useState('')
   const [date, setDate] = useState(null)
-  const [city, setCity] = useState ('')
+  const [city, setCity] = useState('')
   const [phone, setPhoneNumber] = useState('')
-  const [postal, setPostal] = useState ('')
+  const [postal, setPostal] = useState('')
   const [google, setGoogle] = useState('')
   const [country, setCountry] = useState('')
   const [address, setAddress] = useState('')
@@ -80,10 +81,8 @@ const StepperAlternativeLabel = () => {
   const [firstName, setFirstName] = useState('')
   const [activeStep, setActiveStep] = useState(0)
   const [nationality, setNationality] = useState('')
- 
-  
-
-
+  const [education, setEducation] = useState('')
+  const [skills, setSkills] = useState('')
 
   // Handle Stepper
   const handleBack = () => {
@@ -113,164 +112,178 @@ const StepperAlternativeLabel = () => {
     setPhoneNumber('')
     setAddress('')
     setNationality('')
+    setEducation('')
+    setSkills('')
   }
 
   const getStepContent = step => {
     switch (step) {
       case 0:
         return (
-         
-           <Fragment key={step}>
-           <Grid item xs={12} sm={3}>
-             <TextField
-               fullWidth
-               label='First Name'
-               placeholder='Leonard'
-               value={firstName}
-               onChange={e => setFirstName(e.target.value)}
-             />
-           </Grid>
-           <Grid item xs={12} sm={3}>
-             <TextField
-               fullWidth
-               label='Last Name'
-               placeholder='Carter'
-               value={lastName}
-               onChange={e => setLastName(e.target.value)}
-             />
-           </Grid>
-           
-           <Grid item xs={12} sm={6}>
-           <TextField
-               fullWidth
-               label='Address'
-               value={address}
-               onChange={e => setAddress(e.target.value)}
-             />
-           </Grid>
+          <Fragment key={step}>
+            <Grid item xs={12} sm={3}>
+              <TextField
+                fullWidth
+                label='First Name'
+                placeholder='Leonard'
+                value={firstName}
+                onChange={e => setFirstName(e.target.value)}
+              />
+            </Grid>
+            <Grid item xs={12} sm={3}>
+              <TextField
+                fullWidth
+                label='Last Name'
+                placeholder='Carter'
+                value={lastName}
+                onChange={e => setLastName(e.target.value)}
+              />
+            </Grid>
 
-           <Grid item xs={12} sm={6}>
-             <TextField
-               fullWidth
-               label='City'
-               placeholder='Cagayan De Oro City'
-               value={city}
-               onChange={e => setCity(e.target.value)}
-             />
-           </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField fullWidth label='Address' value={address} onChange={e => setAddress(e.target.value)} />
+            </Grid>
 
-           
-           <Grid item xs={12} sm={2}>
-           <TextField
-               fullWidth
-               label='Postal Code'
-               placeholder='9000'
-               value={postal}
-               onChange={e => setPostal(e.target.value)}
-             />
-           </Grid>
-           <Grid item xs={12} sm={2}>
-           <TextField
-               fullWidth
-               label='Nationality'
-               placeholder='Filipino'
-               value={nationality}
-               onChange={e => setNationality(e.target.value)}
-             />
-           </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                fullWidth
+                label='City'
+                placeholder='Cagayan De Oro City'
+                value={city}
+                onChange={e => setCity(e.target.value)}
+              />
+            </Grid>
 
-           <Grid item xs={12} sm={2}>
-                  <DatePicker
-                    selected={date}
-                    showYearDropdown
-                    showMonthDropdown
-                    id='form-layouts-tabs-date'
-                    placeholderText='MM-DD-YYYY'
-                    customInput={<CustomInput />}
-                    onChange={date => setDate(date)}
-                  />
-                </Grid>
+            <Grid item xs={12} sm={2}>
+              <TextField
+                fullWidth
+                label='Postal Code'
+                placeholder='9000'
+                value={postal}
+                onChange={e => setPostal(e.target.value)}
+              />
+            </Grid>
+            <Grid item xs={12} sm={2}>
+              <TextField
+                fullWidth
+                label='Nationality'
+                placeholder='Filipino'
+                value={nationality}
+                onChange={e => setNationality(e.target.value)}
+              />
+            </Grid>
 
-                
-           <Grid item xs={12} sm={3}>
-             <FormControl fullWidth>
-               <InputLabel id='stepper-alternative-personal-select-label'>Country</InputLabel>
-               <Select
-                 label='Country'
-                 value={country}
-                 id='stepper-alternative-personal-select'
-                 onChange={e => setCountry(e.target.value)}
-                 labelId='stepper-alternative-personal-select-label'
-               >
-                 <MenuItem value='Philippines'>Philippines</MenuItem>
-                 <MenuItem value='UK'>UK</MenuItem>
-                 <MenuItem value='USA'>USA</MenuItem>
-                 <MenuItem value='Australia'>Australia</MenuItem>
-                 <MenuItem value='Germany'>Germany</MenuItem>
-               </Select>
-             </FormControl>
-           </Grid>
-           <Grid item xs={12} sm={3}>
-             <TextField
-               fullWidth
-               label='Phone Number'
-               placeholder='+63'
-               value={phone}
-               onChange={e => setPhoneNumber(e.target.value)}
-             />
-             </Grid>
-           
+            <Grid item xs={12} sm={2}>
+              <DatePicker
+                selected={date}
+                showYearDropdown
+                showMonthDropdown
+                id='form-layouts-tabs-date'
+                placeholderText='MM-DD-YYYY'
+                customInput={<CustomInput />}
+                onChange={date => setDate(date)}
+              />
+            </Grid>
 
-         </Fragment>
+            <Grid item xs={12} sm={3}>
+              <FormControl fullWidth>
+                <InputLabel id='stepper-alternative-personal-select-label'>Country</InputLabel>
+                <Select
+                  label='Country'
+                  value={country}
+                  id='stepper-alternative-personal-select'
+                  onChange={e => setCountry(e.target.value)}
+                  labelId='stepper-alternative-personal-select-label'
+                >
+                  <MenuItem value='Philippines'>Philippines</MenuItem>
+                  <MenuItem value='UK'>UK</MenuItem>
+                  <MenuItem value='USA'>USA</MenuItem>
+                  <MenuItem value='Australia'>Australia</MenuItem>
+                  <MenuItem value='Germany'>Germany</MenuItem>
+                </Select>
+              </FormControl>
+            </Grid>
+            <Grid item xs={12} sm={3}>
+              <TextField
+                fullWidthz
+                label='Phone Number'
+                placeholder='+63'
+                value={phone}
+                onChange={e => setPhoneNumber(e.target.value)}
+              />
+            </Grid>
+          </Fragment>
         )
       case 1:
         return (
           <Fragment>
-          <Grid item xs={12} sm={6}>
-            <TextField
-              fullWidth
-              label='Job Title'
-              value={username}
-              placeholder='carterLeonard'
-              onChange={e => setUsername(e.target.value)}
-            />
-          </Grid>
-          <Grid item xs={12} sm={6}>
-            <TextField
-              fullWidth
-              type='email'
-              label='Employer'
-              value={email}
-              placeholder='carterleonard@gmail.com'
-              onChange={e => setEmail(e.target.value)}
-            />
-          </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                fullWidth
+                label='Job Title'
+                value={username}
+                placeholder='carterLeonard'
+                onChange={e => setUsername(e.target.value)}
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                fullWidth
+                type='email'
+                label='Employer'
+                value={email}
+                placeholder='carterleonard@gmail.com'
+                onChange={e => setEmail(e.target.value)}
+              />
+            </Grid>
 
-          <Grid item xs={12} sm={2}>
-                  <DatePicker
-                    selected={date}
-                    showYearDropdown
-                    showMonthDropdown
-                    id='form-layouts-tabs-date'
-                    placeholderText='MM-DD-YYYY'
-                    customInput={<StartandEnd />}
-                    onChange={date => setDate(date)}
-                  />
-                </Grid>
+            <Grid item xs={12} sm={2}>
+              <DatePicker
+                selected={date}
+                showYearDropdown
+                showMonthDropdown
+                id='form-layouts-tabs-date'
+                placeholderText='MM-DD-YYYY'
+                customInput={<StartandEnd />}
+                onChange={date => setDate(date)}
+              />
+            </Grid>
 
-                <Grid item xs={12} sm={6}>
-             <TextField
-               fullWidth
-               label='City'
-               placeholder='Cagayan De Oro City'
-               value={city}
-               onChange={e => setCity(e.target.value)}
-             />
-           </Grid>
-          
-        </Fragment>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                fullWidth
+                label='City'
+                placeholder='Cagayan De Oro City'
+                value={city}
+                onChange={e => setCity(e.target.value)}
+              />
+            </Grid>
+          </Fragment>
         )
       case 2:
+        return (
+          <Fragment key={step}>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                fullWidth
+                label='Education'
+                placeholder=''
+                value={education}
+                onChange={e => setEducation(e.target.value)}
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                fullWidth
+                label='Skills'
+                placeholder=''
+                value={skills}
+                onChange={e => setEducation(e.target.value)}
+              />
+            </Grid>
+          </Fragment>
+        )
+      case 3:
         return (
           <Fragment key={step}>
             <Grid item xs={12} sm={6}>
