d3aabf9522c879c913201fa5fba16456f232b7a7
Lowell Bacalso <bacalso318@gmail.com>
2023-11-15 15:20:08 +0800

    salary slider

--- Joblynx/src/views/pages/job/find_job/JobFilters.js
+++ Joblynx/src/views/pages/job/find_job/JobFilters.js
@@ -1,30 +1,34 @@
 import React, { useState } from 'react'
-import { Grid, CardContent, FormGroup, Container, Checkbox, FormControlLabel, Typography, Slider } from '@mui/material'
+import {
+  Grid,
+  CardContent,
+  FormGroup,
+  Container,
+  Checkbox,
+  FormControlLabel,
+  Typography,
+  Slider,
+  Select,
+  MenuItem
+} from '@mui/material'
 
 const JobFilters = () => {
-  const [from_salary, setFrom_Salary] = useState('')
-  const [to_salary, setTo_Salary] = useState('')
-  const [fullTime, setFullTime] = useState(false)
-  const [partTime, setPartTime] = useState(false)
-  const [internship, setInternship] = useState(false)
-  const [remote, setRemote] = useState(false)
-  const [contract, setContract] = useState(false)
-
   const [selectedCheckbox, setSelectedCheckbox] = useState('')
+  const [salaryType, setSalaryType] = useState('Select')
+  const [hourlyRange, setHourlyRange] = useState([50, 1000])
+  const [monthlyRange, setMonthlyRange] = useState([9000, 150000])
+  const [annuallyRange, setAnnuallyRange] = useState([150000, 1000000])
 
-  // Handle search and filter changes
-  const handleSearch = () => {
-    // Implement your search and filter logic here
-    // Update jobListings based on search criteria
-  }
   const handleCheckboxChange = (event, checkboxName) => {
     if (selectedCheckbox === checkboxName) {
-      // If the same checkbox is clicked again, deselect it
       setSelectedCheckbox('')
     } else {
-      // Otherwise, select the new checkbox and deselect the previously selected one
       setSelectedCheckbox(checkboxName)
     }
+  }
+
+  const handleSalaryTypeChange = event => {
+    setSalaryType(event.target.value)
   }
 
   return (
@@ -36,7 +40,7 @@ const JobFilters = () => {
             <CardContent>
               <Grid item xs={12} sm={6} md={6}>
                 <Typography variant='h6' fontWeight={'bold'}>
-                  Job Status
+                  Job Type
                 </Typography>
                 <FormGroup sx={{}}>
                   <FormControlLabel
@@ -103,31 +107,98 @@ const JobFilters = () => {
                 </FormGroup>
               </Grid>
               <Grid item xs={12} sm={6} md={10} sx={{ width: '200px', mt: '2em' }}>
-                <Typography variant='h6' fontWeight={'bold'}>
-                  Salary
+                <Typography variant='h6' fontWeight='bold'>
+                  Salary Type
                 </Typography>
-                <Typography>From: {from_salary > 0 && <span>${from_salary}</span>}</Typography>
-                <Slider
-                  value={from_salary}
-                  onChange={(_, newValue) => setFrom_Salary(newValue)}
-                  valueLabelDisplay='auto'
-                  valueLabelFormat={value => `$${value}`}
-                  min={100}
-                  max={5000}
-                  step={100}
-                  sx={{ width: '13rem' }}
-                />
-                <Typography>To: {to_salary > 0 && <span>${to_salary}</span>}</Typography>
-                <Slider
-                  value={to_salary}
-                  onChange={(_, newValue) => setTo_Salary(newValue)}
-                  valueLabelDisplay='auto'
-                  valueLabelFormat={value => `$${value}`}
-                  min={100}
-                  max={5000}
-                  step={100}
-                  sx={{ width: '13rem' }}
-                />
+                <Select value={salaryType} onChange={handleSalaryTypeChange}>
+                  <MenuItem value='Select' disabled sx={{ display: 'none' }}>
+                    Select
+                  </MenuItem>
+                  <MenuItem value='hourly'>Hourly</MenuItem>
+                  <MenuItem value='monthly'>Monthly</MenuItem>
+                  <MenuItem value='annually'>Annually</MenuItem>
+                </Select>
+                {salaryType === 'hourly' && (
+                  <>
+                    <Typography sx={{ position: 'fixed' }}>
+                      Paying: {hourlyRange[0] > 0 && <span>${hourlyRange[0]}</span>}{' '}
+                      {hourlyRange[1] > 0 && (
+                        <span style={{ display: 'block' }}>
+                          To: ${hourlyRange[1]}
+                          {hourlyRange[1] === 1000 && <span>+</span>}
+                          <Typography variant='h7' fontWeight={'bold'} display={'block'} mt={'1px'}>
+                            Salary Range
+                          </Typography>
+                        </span>
+                      )}
+                    </Typography>
+                    <Slider
+                      value={hourlyRange}
+                      onChange={(_, newValue) => setHourlyRange(newValue)}
+                      valueLabelDisplay='auto'
+                      valueLabelFormat={value => `$${value}`}
+                      min={50}
+                      max={1000}
+                      step={50}
+                      disableSwap
+                      sx={{ mt: '5em' }}
+                    />
+                  </>
+                )}
+                {salaryType === 'monthly' && (
+                  <>
+                    <Typography sx={{ position: 'fixed' }}>
+                      Paying: {monthlyRange[0] > 0 && <span>${monthlyRange[0]}</span>}{' '}
+                      {monthlyRange[1] > 0 && (
+                        <span style={{ display: 'block' }}>
+                          To: ${monthlyRange[1]}
+                          {monthlyRange[1] === 150000 && <span>+</span>}
+                          <Typography variant='h7' fontWeight={'bold'} display={'block'} mt={'1px'}>
+                            Salary Range
+                          </Typography>
+                        </span>
+                      )}
+                    </Typography>
+                    <Slider
+                      value={monthlyRange}
+                      onChange={(_, newValue) => setMonthlyRange(newValue)}
+                      valueLabelDisplay='auto'
+                      valueLabelFormat={value => `$${value}`}
+                      min={9000}
+                      max={150000}
+                      step={1000}
+                      disableSwap
+                      sx={{ mt: '5em' }}
+                    />
+                  </>
+                )}
+                {salaryType === 'annually' && (
+                  <>
+                    <Typography sx={{ position: 'fixed' }}>
+                      Paying: {annuallyRange[0] > 0 && <span>${annuallyRange[0]}</span>}{' '}
+                      {annuallyRange[1] > 0 && (
+                        <span style={{ display: 'block' }}>
+                          To: ${annuallyRange[1]}
+                          {annuallyRange[1] === 1000000 && <span>+</span>}
+                          <Typography variant='h7' fontWeight={'bold'} display={'block'} mt={'1px'}>
+                            Salary Range
+                          </Typography>
+                        </span>
+                      )}
+                    </Typography>
+                    <Slider
+                      value={annuallyRange}
+                      onChange={(_, newValue) => setAnnuallyRange(newValue)}
+                      valueLabelDisplay='auto'
+                      valueLabelFormat={value => `$${value}`}
+                      min={150000}
+                      max={1000000}
+                      step={50000}
+                      disableSwap
+                      sx={{ mt: '5em' }}
+                    />
+                  </>
+                )}
               </Grid>
             </CardContent>
           </Grid>
